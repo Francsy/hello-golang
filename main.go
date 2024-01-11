7f8fa9a7aa4da20cba689abca1915992dbbabb4f
Francsy <francsyludens@gmail.com>
2024-01-11 22:58:56 +0000

    add/variables and functions initialization and declarations

--- main.go
+++ main.go
@@ -2,6 +2,24 @@ package main
 
 import "fmt"
 
+var card string
+
 func main() {
-	fmt.Println("Hi there!")
+
+	/*
+	   One way to declare a variable
+
+	   var card string = "Ace of Spades"
+
+	   Another:
+
+	   card := "Ace of Spades"
+	*/
+	card := "Ace of Spades"
+	card = newCard()
+	fmt.Println(card)
+}
+
+func newCard() string { // Function that return an string type
+	return "Five of Diamonds"
 }
